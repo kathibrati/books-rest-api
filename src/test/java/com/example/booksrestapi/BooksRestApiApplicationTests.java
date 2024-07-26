33b9aaa15c7eb018ae193cf8668006afec0b64aa
Kathleen Letsch <cskathi@web.de>
2024-07-26 23:35:49 +0200

    init commit

---
+++ src/test/java/com/example/booksrestapi/BooksRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booksrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
